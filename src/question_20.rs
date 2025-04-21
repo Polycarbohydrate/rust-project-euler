97bf9cc2d72b7ee772ae97841e54e3e7e34c161f
Polycarbohydrate <169401794+Polycarbohydrate@users.noreply.github.com>
2025-04-21 12:38:33 -0400

    Create question_20.rs

---
+++ src/question_20.rs
@@ -0,0 +1,16 @@
+// https://projecteuler.net/problem=20
+//
+// Used Python to calculate the numbers due to Rust's integer limitations.
+//
+// x = 1
+// for i in range(1, 101):
+//	   x = x * i
+// print(x)
+
+print(x)
+fn main() {
+    let mut numbers = String::from("93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000");
+    let sum: Vec<u128> = numbers.chars().map(|c| c.to_digit(10).unwrap() as u128).collect();
+    println!("{}", sum.iter().sum::<u128>());
+    
+}
