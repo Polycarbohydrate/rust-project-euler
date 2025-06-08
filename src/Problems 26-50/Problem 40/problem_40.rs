05bb0d128a9fc2e8fd133d5306b5f8356083931c
Polycarbohydrate <169401794+Polycarbohydrate@users.noreply.github.com>
2025-06-08 17:42:55 -0400

    Add Problem 40
    - Added my solution file (in Rust) for Project Euler's Problem 40.
    - Added my steps and plan in the README.md file

---
+++ src/Problems 26-50/Problem 40/problem_40.rs
@@ -0,0 +1,22 @@
+// https://projecteuler.net/problem=39
+use std::time::Instant;
+fn main() {
+    let now = Instant::now();
+    let mut storage = Vec::new();
+    for number in 1..1_000_000 {
+        let x = number.to_string().chars().collect::<Vec<_>>();
+        for char in x {
+            storage.push(char);
+        }
+    }
+    let product = storage[0].to_digit(10).unwrap()
+        * storage[9].to_digit(10).unwrap()
+        * storage[99].to_digit(10).unwrap()
+        * storage[999].to_digit(10).unwrap()
+        * storage[9999].to_digit(10).unwrap()
+        * storage[99999].to_digit(10).unwrap();
+    println!("Length of storage: {}", storage.len());
+    println!("Product of digits: {}", product);
+    println!("================================");
+    println!("{} milliseconds of runtime", now.elapsed().as_millis());
+}
